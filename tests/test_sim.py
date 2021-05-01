15f9315632f7f63d3d8d28cd6d7ccbe2533f8037
AlexLee1999 <chunyen.alex.lee@gmail.com>
2021-05-01 12:01:57 +0800

    add test

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -1,5 +1,27 @@
 import pytest
+import os
+from ..simulation_1 import *
 
 
-def test_sim():
-    pass
+def test_sim1_1():
+    prob_11()
+    assert os.path.exists('./fig1_1.jpg')
+    os.system('rm -rf fig1_1.jpg')
+
+
+def test_sim1_2():
+    prob_12()
+    assert os.path.exists('./fig1_2.jpg')
+    os.system('rm -rf fig1_2.jpg')
+
+
+def test_sim2_1():
+    prob_21()
+    assert os.path.exists('./fig2_1.jpg')
+    os.system('rm -rf fig2_1.jpg')
+
+
+def test_sim2_2():
+    prob_22()
+    assert os.path.exists('./fig2_2.jpg')
+    os.system('rm -rf fig2_2.jpg')
