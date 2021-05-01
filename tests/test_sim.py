f48a27f0f8055249605246eb027c627be203419d
AlexLee1999 <chunyen.alex.lee@gmail.com>
2021-05-01 17:59:31 +0800

    Update test_sim.py

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -1,27 +1,27 @@
 import pytest
 import os
-from .. import *
+from .. import simulation_1
 
 @pytest.mark.all
 def test_sim1_1():
-    prob_11()
+    simulation_1.sim1.prob_11()
     assert os.path.exists('./fig1_1.jpg')
     os.system('rm -rf fig1_1.jpg')
 
 @pytest.mark.all
 def test_sim1_2():
-    prob_12()
+    simulation_1.sim1.prob_12()
     assert os.path.exists('./fig1_2.jpg')
     os.system('rm -rf fig1_2.jpg')
 
 @pytest.mark.all
 def test_sim2_1():
-    prob_21()
+    simulation_1.sim1.prob_21()
     assert os.path.exists('./fig2_1.jpg')
     os.system('rm -rf fig2_1.jpg')
 
 @pytest.mark.all
 def test_sim2_2():
-    prob_22()
+    simulation_1.sim1.prob_22()
     assert os.path.exists('./fig2_2.jpg')
     os.system('rm -rf fig2_2.jpg')
